b03f7dd69e42135f536cc581bee69a44fbcd9f3c
Kevin Buchik <kbuchik@gmail.com>
2019-07-20 00:01:52 -0700

    one working button

--- assets/game.js
+++ assets/game.js
@@ -3,45 +3,53 @@
 //displays random number goal 
 $(document).ready(function () {
     console.log(guessNumber);
-    $("#objective").text(guessNumber);})
+    $("#objective").text(guessNumber);
+})
 //generates random number
 var guessNumber = Math.floor(Math.random() * 200 + 100);
 //global variables
 var counter = 0
-var wins= 0;
-var losses= 0;
+var wins = 0;
+var losses = 0;
+
+var diamondButton1 = Math.floor(Math.random() * 10);
+(console.log(diamonds))
+var diamondButton2 = Math.floor(Math.random() * 10);
+var diamondButton3 = Math.floor(Math.random() * 100);
+var diamondButton4 = Math.floor(Math.random() * 100);
 
 
 
-let diamonds = [100, 50, 2]
 //for loop to generate buttons and populates the buttons with a number on load
-for (var i = 0; i < diamonds.length; i++){
+for (var i = 0; i < 4; i++) {
 
-var diamondButton = $("<img>");
-diamondButton.addClass("diamond-button");
+    var diamonds = $("<img>");
+    diamonds.addClass("diamond-button");
 
-diamondButton.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    diamonds.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
 
-diamondButton.attr("data-diamondvalue", diamonds[i]);
-$("#crystals").append(diamondButton);
+    diamonds.attr("data-diamondvalue", diamondButton1,);
+    $("#crystals").append(diamonds);
 
 }
-var reset = function(){
+var reset = function () {
     counter = 0;
     var guessNumber = Math.floor(Math.random() * 200 + 100);
     console.log(guessNumber);
     $(document).ready(function () {
         console.log(guessNumber);
-        $("#objective").text(guessNumber);})
+        $("#objective").text(guessNumber);
+    })
 }
 
-$(".diamond-button").on("click",function(){
+$(".diamond-button").on("click", function () {
 
     var crystalValue = ($(this).attr("data-diamondvalue"));
     crystalValue = parseInt(crystalValue)
     counter += crystalValue;
     console.log(crystalValue);
+    $("#score").text(counter)
     console.log(counter);
     if (counter === guessNumber) {
         wins++
@@ -59,10 +67,5 @@ $(".diamond-button").on("click",function(){
 
 
 //game only registers loss after second click?????
-
-
-
-
-
 
 
