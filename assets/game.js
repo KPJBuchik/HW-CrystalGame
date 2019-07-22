0f2d821cbf7d20b2958e580ea41c11525e343423
Kevin Buchik <kbuchik@gmail.com>
2019-07-22 11:07:04 -0700

    monday

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,4 @@
 
-
 //displays random number goal 
 $(document).ready(function () {
     console.log(guessNumber);
@@ -11,151 +10,172 @@ $(document).ready(function () {
 //generates random number
 var guessNumber = Math.floor(Math.random() * 200 + 100);
 //global variables
-var counter = 0
-let diamondButton1 = Math.floor(Math.random() * 10);
+var counter = 0;
+let diamondButton1 = Math.floor(Math.random() * 100);
 
-let diamondButton2 = Math.floor(Math.random() * 10);
-let diamondButton3 = Math.floor(Math.random() * 100);
+let diamondButton2 = 2;
+let diamondButton3 = 1;
 let diamondButton4 = Math.floor(Math.random() * 100);
-var diamondButton5 = 2
-var diamondButton6 = 3
-var diamondButton7 = 1
+var diamondButton5 = Math.floor(Math.random() * 100);
+var diamondButton6 = Math.floor(Math.random() * 100);
+var diamondButton7 = Math.floor(Math.random() * 100);
 
 var wins = 0;
 var losses = 0;
 
-reset = function () {
-    counter = 0;
-    diamondButton1 = Math.floor(Math.random() * 10);
-    diamondButton2 = Math.floor(Math.random() * 10);
-    diamondButton3 = Math.floor(Math.random() * 100);
-    diamondButton4 = Math.floor(Math.random() * 100);
-    guessNumber = Math.floor(Math.random() * 200 + 100);
-    console.log(guessNumber);
 
-    $(document).ready(function () {
-        console.log(guessNumber);
-        $("#objective").text(guessNumber);
-
-    })
-}
-
-//for loop to generate buttons and populates the buttons with a number on load.
+//this for loop does nothing but 
 //this is inelegant right? 
 for (var i = 0; i < 1; i++) {
-    var diamonds =$("<p>");
+    var diamonds = $("<p>");
     diamonds.addClass("diamond-button diamond-button1")
-    diamonds.attr("A")
+    diamonds.attr("A")//
     diamonds.attr("data-diamondvalue", diamondButton1);
     $("#crystals").append(diamonds)
 
-    var diamonds =$("<p>");
+    var diamonds = $("<p>");
     diamonds.addClass("diamond-button diamond-button2");
     diamonds.attr("B");
     diamonds.attr("data-diamondvalue", diamondButton2);
     $("#crystals").append(diamonds);
 
-    var diamonds =$("<p>");
+    var diamonds = $("<p>");
     diamonds.addClass("diamond-button diamond-button3");
     diamonds.attr("C");
     diamonds.attr("data-diamondvalue", diamondButton3);
     $("#crystals").append(diamonds);
 
-    var diamonds =$("<p>");
+    var diamonds = $("<p>");
     diamonds.addClass("diamond-button diamond-button4");
     diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton4);
     $("#crystals").append(diamonds);
 
-    var diamonds =$("<p>");
+    var diamonds = $("<p>");
     diamonds.addClass("diamond-button diamond-button5");
     diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton5);
     $("#crystals").append(diamonds);
 
-    var diamonds =$("<p>");
+    var diamonds = $("<p>");
     diamonds.addClass("diamond-button diamond-button6");
     diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton6);
     $("#crystals").append(diamonds);
 
-    var diamonds =$("<p>");
+    var diamonds = $("<p>");
     diamonds.addClass("diamond-button diamond-button7");
     diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton7);
     $("#crystals").append(diamonds);
- 
 
 
-$(".diamond-button1").click(function () {
-    var audio = new Audio('assets/piano-a.wav');
-    audio.play();
-});
+
+    $(".diamond-button1").click(function () {
+        var audio = new Audio('assets/piano-a.wav');
+        audio.play();
+    });
 
 
-$(".diamond-button2").click(function () {
-    var audio = new Audio('assets/piano-b.wav');
-    audio.play();
-});
+    $(".diamond-button2").click(function () {
+        var audio = new Audio('assets/piano-b.wav');
+        audio.play();
+    });
 
-$(".diamond-button3").click(function () {
-    var audio = new Audio('assets/piano-c.wav');
-    audio.play();
-});
+    $(".diamond-button3").click(function () {
+        var audio = new Audio('assets/piano-c.wav');
+        audio.play();
+    });
 
-$(".diamond-button4").click(function () {
-    var audio = new Audio('assets/piano-d.wav');
-    audio.play();
-});
+    $(".diamond-button4").click(function () {
+        var audio = new Audio('assets/piano-d.wav');
+        audio.play();
+    });
 
-$(".diamond-button5").click(function () {
-    var audio = new Audio('assets/piano-e.wav');
-    audio.play();
-});
+    $(".diamond-button5").click(function () {
+        var audio = new Audio('assets/piano-e.wav');
+        audio.play();
+    });
 
-$(".diamond-button6").click(function () {
-    var audio = new Audio('assets/piano-f.wav');
-    audio.play();
-});
-$(".diamond-button7").click(function () {
-    var audio = new Audio('assets/piano-highg.wav');
-    audio.play();
-});
+    $(".diamond-button6").click(function () {
+        var audio = new Audio('assets/piano-f.wav');
+        audio.play();
+    });
+    $(".diamond-button7").click(function () {
+        var audio = new Audio('assets/piano-highg.wav');
+        audio.play();
+    });
 
 
-/* //how to respond to keys
-function myFunction(event) {
-    var x = event.charCode || event.keyCode;
-    if (x == 111 || x == 79) { // o is 111, O is 79
-      alert("You pressed the 'O' key!");
-    }
-  }
+
+/*
+    $(".diamond-button1").onkeydown(function(e) {
+        switch (e.which){
+        case 40:
+        var audio = new Audio('assets/piano-a.wav');
+        audio.play();
+        };
+    });
+
 */
+    
 
 
-$(".diamond-button").on("click", function () {
-    var crystalValue = ($(this).attr("data-diamondvalue"));
-    crystalValue = parseInt(crystalValue)
-    counter += crystalValue;
-    console.log(crystalValue);
-    $("#score").text(counter)
-    console.log(counter);
-    if (counter === guessNumber) {
-        reset();
-        wins++
-        // alert("You win!");
-        $("#wins-column").text(wins);  
+   reset = function () {
+    counter = 0;
+    diamondButton1 = Math.floor(Math.random() * 100);
+    console.log(diamondButton1)
+    diamondButton4 = Math.floor(Math.random() * 100);
+    diamondButton5 = Math.floor(Math.random() * 100);
+    diamondButton6 = Math.floor(Math.random() * 100);
+    diamondButton7 = Math.floor(Math.random() * 100);
+    guessNumber = Math.floor(Math.random() * 200 + 100);
+    console.log(guessNumber);
+    $(document).ready(function () {
+        console.log(guessNumber);
+        $("#objective").text(guessNumber);
+   
 
-    }
-    else if (counter >= guessNumber) {
-        reset();
-        losses++;
-        // alert("You Lose!")
-        $("#losses-column").text(losses); 
-        reset();
+    })
+}
 
-    }
-});
+
+
+    $(".diamond-button").on("click", function () {
+        var crystalValue = ($(this).attr("data-diamondvalue"));
+        crystalValue = parseInt(crystalValue)
+        counter += crystalValue;
+        console.log(crystalValue);
+        $("#score").text(counter)
+        console.log(counter);
+
+
+        
+    
+
+        if (counter === guessNumber) {
+            reset();
+            wins++
+            // alert("You win!");
+            $("#wins-column").text(wins);
+  
+
+        }
+        else if (counter >= guessNumber) {
+            
+            losses++;
+            // alert("You Lose!")
+            $("#losses-column").text(losses);
+
+            reset();
+            
+
+        }
+
+       
+        
+
+    });
 
 }
 //RESET
