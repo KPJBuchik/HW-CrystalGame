c39083621e24feba621d53d6f61ff0530f2bcee3
Kevin Buchik <kbuchik@gmail.com>
2019-07-21 17:04:53 -0700

    changed images to text elements

--- assets/game.js
+++ assets/game.js
@@ -43,79 +43,50 @@ reset = function () {
 //for loop to generate buttons and populates the buttons with a number on load.
 //this is inelegant right? 
 for (var i = 0; i < 1; i++) {
-    var node = document.createTextNode("A");
-    var diamonds = $("<div>");
-    diamonds.addClass("diamond-button diamond-button1");
-
-   
-
-
+    var diamonds =$("<p>");
+    diamonds.addClass("diamond-button diamond-button1")
+    diamonds.attr("A")
     diamonds.attr("data-diamondvalue", diamondButton1);
-    $("#crystals").append(diamonds);
+    $("#crystals").append(diamonds)
 
-    var diamonds = $("<img>");
+    var diamonds =$("<p>");
     diamonds.addClass("diamond-button diamond-button2");
-
-    diamonds.attr("src", "https://i5.walmartimages.com/asr/500c6955-8486-4e4e-94d0-62e4803c6b11_1.6ba763f3e6883a709c205e504e5a4589.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF");
-
-
+    diamonds.attr("B");
     diamonds.attr("data-diamondvalue", diamondButton2);
     $("#crystals").append(diamonds);
 
-    var diamonds = $("<img>");
+    var diamonds =$("<p>");
     diamonds.addClass("diamond-button diamond-button3");
-
-    diamonds.attr("src", "https://images-na.ssl-images-amazon.com/images/I/51FAEEZTvIL._UY395_.jpg");
-
-
+    diamonds.attr("C");
     diamonds.attr("data-diamondvalue", diamondButton3);
     $("#crystals").append(diamonds);
 
-    var diamonds = $("<img>");
+    var diamonds =$("<p>");
     diamonds.addClass("diamond-button diamond-button4");
-
-    diamonds.attr("src", "https://i5.walmartimages.com/asr/ba889e73-c026-43ab-b8f7-bca8334836e6_1.5c1cd9518c4b29f4b0e7acc304110e49.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF");
-
-
+    diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton4);
     $("#crystals").append(diamonds);
 
-    var diamonds = $("<img>");
+    var diamonds =$("<p>");
     diamonds.addClass("diamond-button diamond-button5");
-
-    diamonds.attr("src", "https://images-na.ssl-images-amazon.com/images/I/51JXI%2BtSuRL._UY395_.jpg");
-
-
+    diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton5);
     $("#crystals").append(diamonds);
 
-    var diamonds = $("<img>");
+    var diamonds =$("<p>");
     diamonds.addClass("diamond-button diamond-button6");
-
-    diamonds.attr("src", "https://images-na.ssl-images-amazon.com/images/I/51CbypSfqvL._UY395_.jpg");
-
-
+    diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton6);
     $("#crystals").append(diamonds);
 
-    var diamonds = $("<img>");
+    var diamonds =$("<p>");
     diamonds.addClass("diamond-button diamond-button7");
-
-    diamonds.attr("src", "https://images-na.ssl-images-amazon.com/images/I/51hoKgv3T6L._UY395_.jpg");
-
-
+    diamonds.attr("D");
     diamonds.attr("data-diamondvalue", diamondButton7);
     $("#crystals").append(diamonds);
-}
-/*
-$(".diamond-button5").click(function(){
-   $(".diamond-button5").hide(4000);
-});
+ 
 
-$(".diamond-button1").click(function(){
-   $(".diamond-button1").hide(4000);
-});   
-*/
+
 $(".diamond-button1").click(function () {
     var audio = new Audio('assets/piano-a.wav');
     audio.play();
@@ -158,6 +129,9 @@ function myFunction(event) {
     }
   }
 // only resets counter and guessNumber
+
+
+
 $(".diamond-button").on("click", function () {
     var crystalValue = ($(this).attr("data-diamondvalue"));
     crystalValue = parseInt(crystalValue)
@@ -182,7 +156,7 @@ $(".diamond-button").on("click", function () {
     }
 });
 
-
+}
 //make buttons disapear occasionally?
 //RESET
 
